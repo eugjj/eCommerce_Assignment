e1d86a7fd7ad2b8e1112b2c3e50e6e81c969c98d
thirishweyeewin <thirishweyeewin1@gmail.com>
2022-04-09 15:41:46 +0800

    9 APR 2022 3:41PM added search functionality

--- Controllers/ProductGalleryController.cs
+++ Controllers/ProductGalleryController.cs
@@ -69,6 +69,7 @@ namespace eCommence_Assignment.Controllers
             return View();
         }
 
+        // Detail Page
         public IActionResult ProductDetail(Guid id)
         {
             Products product = db.Products.FirstOrDefault(x => x.Id == id);
@@ -76,6 +77,16 @@ namespace eCommence_Assignment.Controllers
             ViewData["product"] = product;
 
             return View();
+        }
+
+        // Search bar
+        [HttpPost]
+        public IActionResult Search(string search)
+        {
+            List<Products> products = db.Products.Where(x => x.Name.Contains(search) ||
+            x.Description.Contains(search) || x.Brand.Contains(search)).ToList();
+            ViewData["data"] = products;
+            return View("Index");
         }
     }
 }
