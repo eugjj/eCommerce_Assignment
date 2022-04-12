99cac70c4eafff2afc16d945e5d551d94c13a860
Eugene Ong <e0941680@u.nus.edu>
2022-04-12 13:58:19 +0800

    .

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -17,6 +17,16 @@
 
     need to use AJAX to update the cart number on the top right of screen page (any idea how?)*/
 
+
+    let elem = document.getElementById("abc");
+    elem.onmouseover = function () {
+        elem.style.border = "2px solid #29b6f6";
+        elem.style.backgroundColor = "#e1f5fe";
+    }
+    elem.onmouseout = function () {
+        elem.style.border = "";
+        elem.style.backgroundColor = "";
+    }
 }
 
 
