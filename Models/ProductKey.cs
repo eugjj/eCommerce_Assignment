3bc7127ef3ca13c59afac46b4253a316db5714c0
JavstRanger <103159555+JavstRanger@users.noreply.github.com>
2022-04-12 17:14:28 +0800

    Add files via upload

---
+++ Models/ProductKey.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommence_Assignment.Models
+{
+    public class ProductKey
+    {
+        public ProductKey()
+        {
+            Id = new Guid();                     
+        }
+        /* maps to primary key */
+        public Guid Id { get; set; } 
+        // generated product in string format
+        public string PKey { get; set; }
+        // timestamp to be used for show user date of purchase
+        public long CreateTimestamp { get; set; }
+        public virtual ICollection<Products> Products { get; set; }
+        public virtual ICollection<Users> User { get; set; }
+    }
+}
