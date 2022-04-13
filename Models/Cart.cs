fb3105b5d66da2c34437dd051116274784572c8c
thirishweyeewin <thirishweyeewin1@gmail.com>
2022-04-14 00:29:51 +0800

    12:29 AM APR 14
    Added AddToCart functionality

---
+++ Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommence_Assignment.Models
+{
+    public class Cart
+    {
+        public Cart()
+        {
+            Id = new Guid();
+        }
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid ProductId { get; set; }
+    }
+}
