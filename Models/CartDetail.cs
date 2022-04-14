9523194559c8efad47b9d894b5cd33387b8139dc
LAUYUNHOU <51911049+LAUYUNHOU@users.noreply.github.com>
2022-04-14 11:19:00 +0800

    Add files via upload

---
+++ Models/CartDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace eCommence_Assignment.Models
+
+public class CartDetail
+{
+	public string CartId { get; set; }
+	public int ProductId { get; set; }
+	public string UserId { get; set; }
+	public int Quantity { get; set; }
+
+	public virtual Products Products { get; set; }
+}
