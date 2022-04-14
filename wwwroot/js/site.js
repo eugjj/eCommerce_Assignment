16ff3fc5da6a73c3feb314f0ad6e48d654ba8387
Eugene Ong <e0941680@u.nus.edu>
2022-04-14 12:46:32 +0800

    updated topbar styling

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,9 @@
 ﻿window.onload = function () {
 
-    if (window.location.href.indexOf("Login") > -1 && ) {
-
+    if (window.location.href.indexOf("Login") > -1) {
+        document.getElementById("hide").innerHTML = ""
     }
 
-    /*For 'add to cart' button in the gallery page
-
-    let addCart = document.getElementById("@productId");
-    if (!addClick)
-    {
-        return;
-    }
-    addCart.onClick function()
-    {
-        
-    }
-
-    need to use AJAX to update the cart number on the top right of screen page (any idea how?)
 
 }
 
